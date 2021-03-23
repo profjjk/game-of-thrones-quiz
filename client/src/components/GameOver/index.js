89f13853b5e3f35f01a8e8ef0b3ad77ea461ed98
Jordan Kelly <profjjk@gmail.com>
2021-03-23 13:30:09 -0700

    record userName input

--- client/src/components/GameOver/index.js
+++ client/src/components/GameOver/index.js
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import './index.css';
 
 function GameOver() {
+  const inputRef = useRef();
 
+  const handleSubmit = event => {
+    event.preventDefault();
+    console.log(inputRef.current.value)
+  }
+
+  return (
+    <div>
+      <h1 className=" fancy large center">Valar Morghulis</h1>
+      <h4 className="medium center">Score: </h4>
+      <div id="form">
+        <input type="text" placeholder="Enter your name..." ref={inputRef} />
+        <button className="btn submit" onClick={handleSubmit}>Submit</button>
+      </div>
+    </div>
+  )
 }
 
 export default GameOver;
