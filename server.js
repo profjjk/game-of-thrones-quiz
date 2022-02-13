ec7180711abbc3dc43dc9a23b6125d19486a43b0
Jordan Kelly <jordankelly@Jordans-MacBook-Pro.local>
2022-02-13 00:29:30 -0800

    change db name

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ if (process.env.NODE_ENV === "production") {
 
 app.use(routes);
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/thronesDB");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/thrones");
 
 app.listen(PORT, () => {
   console.log("Server listening on PORT " + PORT);
